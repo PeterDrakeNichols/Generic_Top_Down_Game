1502405b3f6ade7054c05569f858ca7097bd4bb0
PeterDrakeNichols <PeterDrakeNichols@gmail.com>
2016-09-12 00:57:28 -0500

    first commit

---
+++ Assets/Players/scripts/PlayerMoveSimple.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoveSimple : MonoBehaviour {
+    // simple point and click movement a la league of legends using the NavMeshAgent component
+
+    private NavMeshAgent navMeshAgent;
+    private MouseVectors mouseVectors;
+    public float stopDuration; //amount of time to stop the player to perform action (i.e spells, heals, foods)
+    void Awake () {
+        navMeshAgent = GetComponent<NavMeshAgent>() as NavMeshAgent;
+        mouseVectors = GetComponent<MouseVectors>() as MouseVectors;
+        
+    }
+
+	void Update () {
+        if (Input.GetMouseButton(1))
+        {
+            RaycastHit mouseOnGround = mouseVectors.MouseToGround();
+            navMeshAgent.SetDestination(mouseOnGround.point);
+        }
+
+    }
+}
---
+++ Assets/Utilities/MouseVectors.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseVectors : MonoBehaviour {
+
+    private Camera mainCamera;
+
+    void Awake() {
+        mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+    //simple raycast that returns mouse position projected to the first collider it hits
+    public RaycastHit MouseToGround() {
+        RaycastHit hit;
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+            return hit;
+        }
+        return hit;
+    }
+    // finds the mouse position x and y on a plane fixed to an object's (most likely a player's) y position
+    public Vector3 MouseToFeet() {
+        Plane plane = new Plane(Vector3.up, new Vector3(0, transform.position.y, 0));
+        float ent;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out ent)) {
+            return ray.GetPoint(ent);
+        }
+        return transform.position;
+    }
+    //normalizes a requested location as a directional vector based on an object's (most likely a player's) position
+    public Vector3 Direction(float magnitude) {
+        return transform.position + Vector3.Normalize(MouseToFeet() - transform.position) * magnitude;
+    }
+    //returns true if selected is within reach of an object (most likely a player)
+    public bool WithInReach(Vector3 location, Vector3 requestedLocation, float reach) {
+        if (requestedLocation.x - location.x <= reach && requestedLocation.z - requestedLocation.z <= reach)
+        {
+            return true;
+        }
+        return false;
+    }
+}
