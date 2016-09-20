5694e11224088c3b98208998f894302515b85dcc
Art Siriwatt <Art Siriwatt>
2016-09-19 22:58:54 -0500

    rename player movement scripts and add parent class

---
+++ Assets/Players/scripts/PlayerMovementScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+abstract public class PlayerMovementScript : MonoBehaviour {
+
+	void test() {
+	
+	}
+
+}
---
+++ Assets/Players/scripts/PlayerMovementWASDScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementWASDScript : PlayerMovementScript {
+    //simple w-a-s-d controls that use the set destination method like a carrot on a stick for the NavMeshAgent Component
+
+    private NavMeshAgent navMeshAgent;
+    void Awake() {
+        navMeshAgent = GetComponent<NavMeshAgent>() as NavMeshAgent;
+    }
+
+	void Update () {
+        navMeshAgent.destination = CalculateDestination();
+	}
+
+     private Vector3 CalculateDestination () {
+        Vector3 destination;
+        destination.x = transform.position.x + Input.GetAxis("Horizontal");
+        destination.z = transform.position.z + Input.GetAxis("Vertical");
+        destination.y = transform.position.y;
+        return destination; 
+    }
+
+    void OnDrawGizmos () {
+        Gizmos.color = Color.red;
+        if (navMeshAgent){
+            Gizmos.DrawSphere(navMeshAgent.destination, .5f);
+        }
+    }
+}
