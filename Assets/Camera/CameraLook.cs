880c8dfd59feb2c293aa68d93de167fd5749ae94
PeterDrakeNichols <PeterDrakeNichols@gmail.com>
2016-09-29 20:52:26 -0500

    pre-merge

---
+++ Assets/Camera/CameraLook.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLook : MonoBehaviour {
+
+    private GameObject playerAvatar;
+    private Vector3 offset;
+
+	void Awake () {
+        playerAvatar = GameObject.FindWithTag("Player");
+        offset = transform.position - playerAvatar.transform.position;
+	}
+
+	void LateUpdate () {
+        transform.position = playerAvatar.transform.position + offset;
+	}
+}
